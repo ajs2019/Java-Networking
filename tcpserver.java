7adfe6efb6148abc69f25ce4f7e4233fa0c7eb25
ajs2019 <118772040+ajs2019@users.noreply.github.com>
2023-02-05 09:56:05 -0500

    Add files via upload

---
+++ tcpserver.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPEchoServer {
+    public static void main(String[] args) throws IOException {
+        int port = 1234;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server listening on port " + port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Accepted connection from " + clientSocket.getInetAddress());
+            new Thread(() -> {
+                try {
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        out.println(inputLine);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+}
